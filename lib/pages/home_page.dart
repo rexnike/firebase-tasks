f2f4845472de36b362e014f2f1bd849ea236b10f
Hector Felipee <rexnike12@gmail.comm>
2022-12-06 12:02:03 -0500

    quinto commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,17 +5,6 @@ class HomePage extends StatelessWidget {
   
   CollectionReference tasksReference = FirebaseFirestore.instance.collection('tasks');
 
-  Stream<int> counter() async*{
-  for(int i=0; i<10; i++){
-    yield i;
-    await Future.delayed(const Duration(seconds:2));
-    } 
-  }
-
-  Future<int> getNumber() async{
-  return 1000;
-}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +13,23 @@ class HomePage extends StatelessWidget {
       ),
 
       body: StreamBuilder(
-        stream: counter(),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
-          if(snapshot.hasData){
-            int data = snapshot.data;
-            return Center(child:  Text(data.toString(), style: TextStyle(fontSize: 50),),);
+        stream: tasksReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if(snap.hasData){
+            QuerySnapshot collection = snap.data;
+            List<QueryDocumentSnapshot> docs = collection.docs;
+            List<Map<String, dynamic>> docsMap = docs.map((e) => e.data() as Map<String, dynamic>).toList();
+            print(docsMap);
+            return ListView.builder(
+              itemCount: docsMap.length,
+              itemBuilder: (BuildContext contex, int index){
+                return ListTile(
+                  title: Text(docsMap[index]["title"]),
+                );
+              },
+            );
           }
           return Center(child: CircularProgressIndicator(),);
-        
         },
       ),
 
