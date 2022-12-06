30745899966522deb50d3b71ef3e3578479b51f2
Hector Felipee <rexnike12@gmail.comm>
2022-12-05 23:14:21 -0500

    second commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  
+  CollectionReference tasksReference = FirebaseFirestore.instance.collection('tasks');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Firebase Firestore"),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: (() {
+                tasksReference.get().then((value){
+                  //QuerySnapshot collection = value;
+                  //List<QueryDocumentSnapshot> docs = collection.docs;
+                  //QueryDocumentSnapshot doc = docs[1];
+                  //print(doc.id);
+                  //print(doc.data());
+
+                  QuerySnapshot collection = value;
+                  collection.docs.forEach((QueryDocumentSnapshot element) {
+                    Map<String, dynamic> myMap = element.data() as Map<String , dynamic>;
+                    print(myMap["title"]);
+                    }
+                  );
+                    }
+                  );
+                }
+              ), 
+              child: Text(
+                "Obtener la data"
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
