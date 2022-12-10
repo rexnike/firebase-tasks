9061c4dfc2735c4188cf7979958cef67e935e569
Hector Felipee <rexnike12@gmail.comm>
2022-12-09 22:04:20 -0500

    noveno commit

---
+++ lib/model/task_model.dart
@@ -0,0 +1,24 @@
+
+class TaskModel {
+  String title;
+  String description;
+  String date;
+  String category;
+  bool status;
+
+  TaskModel({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.category,
+    required this.status,
+  }); 
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) => TaskModel(
+    title: json["title"], 
+    description: json["description"], 
+    date: json["date"], 
+    category: json["category"], 
+    status: json["status"],
+    );
+}
